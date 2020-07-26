a7e717fe0ae2a3a587aeb620883b86cbc2ed2a3f
Tyler Goelz <tyler@tylergoelz.com>
2020-07-25 18:45:49 -0700

    initial commit.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8975cdae66cdea2bdc218d69165d5f98')
+    }
+  }
+})
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5dcba221-6851-48f7-989d-10f7971bf940'
+}
